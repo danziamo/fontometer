dbcbb06668e1d55c3bb19a413a749ecc35edfa83
danziamo <dan.takyrbashev@gmail.com>
2017-10-20 11:24:48 -0700

    Adding test to split names of font

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert')
+    , fu = require('../lib/font-util')
 
 describe('blanc', function() {
 
     describe('first', function() {
         assert.equal(1, 1);
+    });
+
+    describe('formatFont', function() {
+        assert.equal(fu.formatFont('Droid+Sans|Indie+Flower:i|Roboto&subset=cyrillic'), 'Droid Sans, Indie Flower, Roboto');
     });
 });
